35d0e1c0140273fcb1670329c1cae39b57f115b5
StephenWang <stephen.wang.7971@gmail.com>
2020-04-30 12:02:59 +0800

    remove logs

--- src/main/java/com/abc/validators/domain/Restriction.java
+++ src/main/java/com/abc/validators/domain/Restriction.java
@@ -7,9 +7,6 @@ import com.abc.validators.validators.Validator;
 public class Restriction {
 
     public void validate(Tuple anObject) {
-        System.out.println("1=" + anObject.getStudentType());
-        System.out.println("2=" + Student.recognize(anObject.getStudentType()));
-        System.out.println("3=" + Student.recognize(anObject.getStudentType()).getValidator());
         Student.recognize(anObject.getStudentType()).getValidator().validate(anObject);
     }
 
@@ -26,7 +23,6 @@ public class Restriction {
         public static Student recognize(String studentType) {
             for (Student type : Student.values()) {
                 if (type.name().equalsIgnoreCase(studentType)) {
-                    System.out.println("matched:" + type);
                     return type;
                 }
             }
